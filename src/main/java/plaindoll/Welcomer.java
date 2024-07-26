97f308e6256568355143b8980a5097debd653494
tourk <tourk@vagrant>
2024-07-26 08:41:35 -0400

    fix literat in Welcomer.Java

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -2,7 +2,7 @@ package plaindoll;
 
 public class Welcomer{
         public String sayHello() {
-                return "Hi, hi, hi, you are the best hunter"";
+                return "Hi, hi, hi, you are the best hunter";
         }
 	public String sayWelcome() {
 		return "Welcome home, good hunter. What is it your desire?";
